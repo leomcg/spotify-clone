79fd83e956d3698d9093b52c30778e49900bf981
Leonardo Gonçalves <leomcg@gmail.com>
2020-08-16 18:45:10 -0300

    Created Constants class

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -32,7 +32,7 @@ class Account {
 
   private function validateUsername($un) {
     if(strlen($un) > 25 || strlen($un) < 5) {
-      array_push($this->errorArray, 'Your username must be between 5 and 25 characters long');
+      array_push($this->errorArray, Constants::$usernameLength);
       return;
     }
 
@@ -41,26 +41,26 @@ class Account {
 
   private function validateFirstName($fn) {
     if(strlen($fn) > 25 || strlen($fn) < 2) {
-      array_push($this->errorArray, 'Your first name must be between 2 and 25 characters long');
+      array_push($this->errorArray, Constants::$firstNameLength);
       return;
     }
   }
 
   private function validateLastName($ln) {
     if(strlen($ln) > 25 || strlen($ln) < 2) {
-      array_push($this->errorArray, 'Your last name must be between 2 and 25 characters long');
+      array_push($this->errorArray, Constants::$lastNameLength);
       return;
     }
   }
 
   private function validateEmails($em, $em2) {
     if($em !== $em2) {
-      array_push($this->errorArray, 'E-mails don\'t match');
+      array_push($this->errorArray, Constants::$emailsDontMatch);
       return;
     }
 
     if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-      array_push($this->errorArray, 'Invalid e-mail');
+      array_push($this->errorArray, Constants::$invalidEmail);
       return;
     }
 
@@ -69,17 +69,17 @@ class Account {
 
   private function validatePasswords($pw, $pw2) {
     if($pw !== $pw2) {
-      array_push($this->errorArray, 'Passwords don\'t match');
+      array_push($this->errorArray, Constants::$passwordsDontMatch);
       return;
     }
 
     if(preg_match('/[^A-Za-z0-9]/', $pw)) {
-      array_push($this->errorArray, 'Your password can only contain numbers and letters');
+      array_push($this->errorArray, Constants::$invalidPassword);
       return;
     }
 
     if(strlen($pw) > 25 || strlen($pw) < 5) {
-      array_push($this->errorArray, 'Your password must be between 5 and 25 characters');
+      array_push($this->errorArray, Constants::$passwordLength);
       return;
     }
   }
--- register.php
+++ register.php
@@ -1,77 +1,134 @@
-
 <?php
-  include('includes/classes/Account.php');
+include('includes/classes/Account.php');
 
-  $account = new Account();
+$account = new Account();
 
-  include('includes/handlers/register-handler.php');
-  include('includes/handlers/login-handler.php');
+include('includes/handlers/register-handler.php');
+include('includes/handlers/login-handler.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome to Slotify</title>
 </head>
+
 <body>
   <div id="inputContainer">
     <form method="POST" action="register.php" id="loginForm">
       <h2>Login to your account</h2>
       <p>
         <label for="loginUsername">Username</label>
-        <input type="text" id="loginUsername" name="loginUsername" placeholder="e.g. bartSimpson" required>
+        <input 
+          type="text" 
+          id="loginUsername" 
+          name="loginUsername" 
+          placeholder="e.g. bartSimpson" 
+          value="<?php $_POST['loginUsername'] ?>"
+          required>
       </p>
       <p>
         <label for="loginPassword">Password</label>
-        <input type="password" name="loginPassword" id="loginPassword" placeholder="Your password" required>
+        <input 
+          type="password" 
+          id="loginPassword" 
+          name="loginPassword" 
+          placeholder="Your password"
+          value="<?php $_POST['loginPassword'] ?>"
+          required>
       </p>
 
       <button type="submit" name="loginButton">LOG IN</button>
     </form>
-    
+
     <form method="POST" action="register.php" id="registerForm">
       <h2>Create your free account</h2>
       <p>
-        <?php echo $account->getError('Your username must be between 5 and 25 characters long')?>
+        <?php echo $account->getError(Constants::$usernameLength) ?>
         <label for="username">Username</label>
-        <input type="text" id="username" name="username" placeholder="e.g. bartSimpson" required>
+        <input 
+          type="text" 
+          id="username" 
+          name="username" 
+          placeholder="e.g. bartSimpson"
+          value="<?php $_POST['username'] ?>"
+          required>
       </p>
       <p>
-        <?php echo $account->getError('Your first name must be between 2 and 25 characters long')?>
+        <?php echo $account->getError(Constants::$firstNameLength) ?>
         <label for="firstName">First name</label>
-        <input type="text" id="firstName" name="firstName" placeholder="e.g. Bart" required>
+        <input 
+          type="text" 
+          id="firstName" 
+          name="firstName" 
+          placeholder="e.g. Bart" 
+          value="<?php $_POST['firstName'] ?>"
+          required>
       </p>
       <p>
-        <?php echo $account->getError('Your last name must be between 2 and 25 characters long')?>
+        <?php echo $account->getError(Constants::$lastNameLength) ?>
         <label for="lastName">Last name</label>
-        <input type="text" id="lastName" name="lastName" placeholder="e.g. Simpson" required>
+        <input 
+          type="text" 
+          id="lastName" 
+          name="lastName" 
+          placeholder="e.g. Simpson" 
+          value="<?php $_POST['lastName'] ?>"
+          required>
       </p>
       <p>
-        <?php echo $account->getError('Invalid e-mail')?>
+        <?php echo $account->getError(Constants::$invalidEmail) ?>
+        <?php echo $account->getError(Constants::$emailsDontMatch) ?>
         <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" placeholder="e.g. bart@gmail.com" required>
+        <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          placeholder="e.g. bart@gmail.com"
+          value="<?php $_POST['email'] ?>" 
+          required>
       </p>
       <p>
-        <?php echo $account->getError('E-mails don\'t match')?>
-        <label for="email">Confirm E-mail</label>
-        <input type="email" id="email" name="email2" placeholder="Confirm your e-mail" required>
+        <label for="email2">Confirm E-mail</label>
+        <input 
+          type="email" 
+          id="email2" 
+          name="email2" 
+          placeholder="Confirm your e-mail"
+          value="<?php $_POST['email2'] ?>" 
+          required>
       </p>
       <p>
-        <?php echo $account->getError('Your password can only contain numbers and letters')?>
+        <?php echo $account->getError(Constants::$invalidPassword) ?>
+        <?php echo $account->getError(Constants::$passwordLength) ?>
+        <?php echo $account->getError(Constants::$passwordsDontMatch) ?>
         <label for="password">Password</label>
-        <input type="password" name="password" id="password" placeholder="Your password" required>
+        <input 
+          type="password" 
+          id="password" 
+          name="password" 
+          placeholder="Your password" 
+          value="<?php $_POST['password'] ?>"
+          required>
       </p>
       <p>
-        <?php echo $account->getError('Passwords don\'t match')?>
-        <label for="password">Confirm Password</label>
-        <input type="password" name="password2" id="password" placeholder="Confirm your password" required>
+        <label for="password2">Confirm Password</label>
+        <input 
+          type="password" 
+          id="password2" 
+          name="password2" 
+          placeholder="Confirm your password" 
+          value="<?php $_POST['password2'] ?>"
+          required>
       </p>
 
       <button type="submit" name="registerButton">SIGN UP</button>
     </form>
-  
+
   </div>
 </body>
+
 </html>
