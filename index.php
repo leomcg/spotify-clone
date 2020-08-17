2af7ed074e2989d300f85399a4d6a5d8df235982
Leonardo Gonçalves <leomcg@gmail.com>
2020-08-17 17:39:21 -0300

    Implements the Now Playing bar

--- index.php
+++ index.php
@@ -16,9 +16,62 @@ include('includes/config.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Document</title>
 </head>
 <body>
-  hello!
+  <div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+
+      <div id="nowPlayingLeft">
+        <div class="content">
+          <span class="albumLink">
+            <img class="albumArtwork" src="assets/images/album-covers/generic.jpg" alt="generic cover">
+          </span>
+          <div class="trackInfo">
+            <span class="trackName">Happy Birthday</span>
+            <span class="artistName">Leonardo Gonçalves</span>
+          </div>
+        </div>
+      </div>
+
+      <div id="nowPlayingCenter">
+        <div class="content playerControls">
+          <div class="buttons">
+            <button class="controlButton shuffle"><img src="assets/icons/shuffle.png" alt="shuffle"></button>
+            <button class="controlButton previous"><img src="assets/icons/previous.png" alt="previous"></button>
+            <button class="controlButton play"><img src="assets/icons/play.png" alt="play"></button>
+            <button class="controlButton pause" style="display: none" ><img src="assets/icons/pause.png" alt="pause"></button>
+            <button class="controlButton next"><img src="assets/icons/next.png" alt="next"></button>
+            <button class="controlButton repeat"><img src="assets/icons/repeat.png" alt="repeat"></button>
+          </div>
+        </div>
+        <div class="playbackBar">
+          <span class="progressTime current">0.00</span>
+          <div class="progressBar">
+            <div class="progressBarBg">
+              <div class="progress"></div>
+            </div>
+          </div>
+          <span class="progressTime remaining">0.00</span>
+        </div>  
+      </div>
+
+      <div id="nowPlayingRight">
+        <div class="volumeBar">
+          <button class="controlButton volume" title="Volume button">
+            <img src="assets/icons/volume.png" alt="Volume">
+          </button>
+          <div class="progressBar">
+            <div class="progressBarBg">
+              <div class="progress"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
 </body>
 </html>
